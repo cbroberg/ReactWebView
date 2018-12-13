4cbc2aeac8aa33c407c960c1b61bc6d4800249e2
Christian Broberg <cb@cb-pro.local>
2018-12-13 02:34:22 +0100

    First test of WebView very simple function component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,44 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { WebView } from "react-electron-web-view";
+import React from "react"
+import ReactDOM from "react-dom"
 
-import "./styles.css";
+import "./styles.css"
+
+function WebView(props) {
+	return (
+		<div className="WebView">
+			<iframe 
+				src={props.src} 
+				height={props.height} 
+				width={props.width} 
+				scrolling={props.scroll} 
+				frameBorder='0' 
+				marginHeight='0'
+				marginWidth='0'
+				padding='0'
+			/>
+		</div>
+	)
+}
+
+// const URL = `http://localhost:3005/events/add?date=2018-04-21&name=Viola%20Ingeman%20Broberg&birthday=1&content=This%20is%20so%20much%20fun%20to%20work%20with%20the%20MySQL/Express/React%20stack`
+const URL = `http://localhost:3005/event/1`
+// $response->headers->remove('X-Frame-Options')
+// $response -> headers -> set('X-Frame-Options', 'ALLOW-FROM', 'http://widgets.dev/')
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <WebView src="https://www.google.com" />
+	<div className="App">
+      <h1>WebView</h1>
+	  <WebView src="https://console.senti.cloud/login" height="500" width="375" />
+	  <WebView src="https://console.senti.cloud/login" height="500" width="375" scroll="no" />
+	  <WebView src={URL} height="500" width="375" />
     </div>
-  );
+  )
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById("root")
+ReactDOM.render(<App />, rootElement)
+
+// https://www.lifewire.com/iframes-and-css-3468669
